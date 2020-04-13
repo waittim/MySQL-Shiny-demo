b2a961646cfdf76083c4d860556e9a6cc2060f42
Zekun Wang <zekun.wang.us@gmail.com>
2020-04-13 00:52:06 -0500

    create db and insert

---
+++ loan_db.sql
@@ -0,0 +1,176 @@
+-- Name: Jiayi Wang, Zekun Wang
+-- Email: jiayi.wang.2@vanderbilt.edu
+-- Project2
+
+-- create database
+
+DROP DATABASE IF EXISTS loan;
+CREATE DATABASE loan;
+USE loan;
+
+DROP TABLE IF EXISTS raw;
+CREATE TABLE raw(
+id INT(9),
+int_rate DECIMAL(4,2),
+grade char(2),
+loan_amnt INT(9),
+loan_status VARCHAR(20),
+disbursement_method VARCHAR(20)  CHECK (disbursement_method IN ('CASH', 'DIRECT_PAY')),
+title VARCHAR(200),
+zip_code VARCHAR(50),
+funded_amnt VARCHAR(20),
+funded_amnt_inv INT(9),
+initial_list_status VARCHAR(10),
+purpose VARCHAR(50),
+addr_state CHAR(2),
+annual_inc INT(9), 
+verification_status VARCHAR(50), 
+home_ownership VARCHAR(20) CHECK(home_ownership IN ('RENT',' OWN', 'MORTGAGE', 'OTHER')) ,
+last_credit_pull_d VARCHAR(8),
+emp_title VARCHAR(50), 
+emp_length VARCHAR(50), 
+total_acc VARCHAR(10), 
+delinq_2yrs SMALLINT(2), 
+open_acc VARCHAR(20), 
+delinq_amnt VARCHAR(100), 
+earliest_cr_line VARCHAR(100), 
+pub_rec_bankruptcies VARCHAR(100), 
+pub_rec VARCHAR(50),
+mths_since_recent_bc VARCHAR(10),
+num_actv_bc_tl INT(9),
+num_bc_sats INT(9),
+num_bc_tl INT(9),
+total_bc_limit INT(9),
+mo_sin_rcnt_tl INT(5),
+num_accts_ever_120_pd INT(5),
+num_sats INT(5),
+num_tl_120dpd_2m VARCHAR(10),
+num_tl_30dpd INT(5),
+num_tl_90g_dpd_24m INT(5),
+num_tl_op_past_12m INT(5),
+tot_cur_bal INT(9),
+inqury_id INT(9),
+inq_fi VARCHAR(10),
+inq_last_12m VARCHAR(20),
+mths_since_recent_inq VARCHAR(20),
+installment DECIMAL(9,3),
+mo_sin_old_il_acct VARCHAR(10),
+mths_since_rcnt_il VARCHAR(10),
+num_il_tl INT(9)
+);
+
+
+LOAD DATA INFILE '/Users/waittim/Documents/MySQL/MySQL-Shiny-demo/data/loan.csv'
+INTO TABLE raw
+CHARACTER SET latin1
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'; 
+
+
+-- create tables with UML diagrams
+
+-- loan TABLE
+DROP TABLE IF EXISTS loan;
+CREATE TABLE loan(
+id INT(9) PRIMARY KEY,
+int_rate DECIMAL(4,2),
+grade CHAR(2),
+loan_amnt INT(9),
+loan_status VARCHAR(20),
+disbursement_method VARCHAR(20)  CHECK (disbursement_method IN ('CASH', 'DIRECT_PAY')),
+title VARCHAR(200),
+zip_code VARCHAR(50),
+funded_amnt VARCHAR(20),
+funded_amnt_inv INT(9),
+initial_list_status VARCHAR(10) CHECK(initial_list_status IN ('W','F')),
+purpose VARCHAR(50)
+);
+
+INSERT INTO loan
+SELECT id, int_rate, grade,loan_amnt,loan_status,disbursement_method,title,zip_code,funded_amnt,funded_amnt_inv,initial_list_status,purpose
+FROM raw;
+
+-- borrower table
+DROP TABLE IF EXISTS borrower;
+CREATE TABLE borrower(
+id INT(9),
+addr_state CHAR(2),
+annual_inc INT(9), 
+verification_status VARCHAR(50), 
+home_ownership VARCHAR(20) CHECK(home_ownership IN ('RENT',' OWN', 'MORTGAGE', 'OTHER')) ,
+last_credit_pull_d VARCHAR(8),
+emp_title VARCHAR(50), 
+emp_length VARCHAR(50), 
+total_acc VARCHAR(10), 
+delinq_2yrs SMALLINT(2), 
+open_acc VARCHAR(20), 
+delinq_amnt VARCHAR(100), 
+earliest_cr_line VARCHAR(100), 
+pub_rec_bankruptcies VARCHAR(100), 
+pub_rec VARCHAR(50)
+);
+
+INSERT INTO borrower
+SELECT id,addr_state,annual_inc,verification_status,home_ownership,last_credit_pull_d,emp_title,emp_length,total_acc,delinq_2yrs,open_acc,delinq_amnt,earliest_cr_line,pub_rec_bankruptcies,pub_rec
+FROM raw;
+
+-- bankcard table
+DROP TABLE IF EXISTS bankcard;
+CREATE TABLE bankcard(
+id INT(9) PRIMARY KEY,
+mths_since_recent_bc INT(9),
+num_actv_bc_tl INT(9),
+num_bc_sats INT(9),
+num_bc_tl INT(9),
+total_bc_limit INT(9)
+);
+
+INSERT INTO bankcard
+SELECT id,mths_since_recent_bc,num_actv_bc_tl,num_bc_sats,num_bc_tl,total_bc_limit
+FROM raw;
+
+
+-- bank_accounts
+DROP TABLE IF EXISTS bank_accounts;
+CREATE TABLE bank_accounts(
+id INT(9) PRIMARY KEY,
+mo_sin_rcnt_tl INT(5),
+num_accts_ever_120_pd INT(5),
+num_sats INT(5),
+num_tl_120dpd_2m VARCHAR(10),
+num_tl_30dpd INT(5),
+num_tl_90g_dpd_24m INT(5),
+num_tl_op_past_12m INT(5),
+tot_cur_bal INT(9)
+);
+
+INSERT INTO bank_accounts
+SELECT id,mo_sin_rcnt_tl,num_accts_ever_120_pd,num_sats,num_tl_120dpd_2m,num_tl_30dpd,num_tl_90g_dpd_24m,num_tl_op_past_12m,tot_cur_bal
+FROM raw;
+
+-- inquries table
+DROP TABLE IF EXISTS inquries;
+CREATE TABLE inquires(
+inqury_id INT(9) PRIMARY KEY,
+inq_fi VARCHAR(10),
+inq_last_12m VARCHAR(20),
+mths_since_recent_inq VARCHAR(20)
+);
+
+INSERT INTO bank_accounts
+SELECT inqury_id,inq_fi,inq_last_12m,mths_since_recent_inq
+FROM raw;
+
+-- installment table
+DROP TABLE IF EXISTS installment;
+CREATE TABLE installment(
+id iNT(9) PRIMARY KEY,
+installment DECIMAL(9,3),
+mo_sin_old_il_acct VARCHAR(10),
+mths_since_rcnt_il VARCHAR(10),
+num_il_tl INT(9)
+);
+
+INSERT INTO installment
+SELECT id,installment,mo_sin_old_il_acct,mths_since_rcnt_il,num_il_tl
+FROM raw;
